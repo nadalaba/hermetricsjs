f31052c66f6606d60e405332d553982434d4dee0
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-08-19 01:38:48 +0300

    test: add testing for transposition in damerau-levenshtein

--- tests/unit/damerau_levenshteinl.test.ts
+++ tests/unit/damerau_levenshteinl.test.ts
@@ -6,19 +6,35 @@ describe('Damerau - Levenshtein Distance', function()
 {
     describe('Distance Test', function()
     {
+        // 1 transposition
+        it('should return 2 for ab - ba', function()
+        {   
+            const dam = new DamerauLevenshtein();
+            const distance = dam.distance('ab', 'ba');
+            expect(distance).equal(1);
+        });
+        // 1 transposition and 1 deletion (unrestricted)
+        it('should return 2 for abc - ca', function()
+        {   
+            const dam = new DamerauLevenshtein();
+            const distance = dam.distance('abc', 'ca');
+            expect(distance).equal(2);
+        });
+        // 2 substitutions
         it('should return 2 for abcd - cbad', function()
         {   
             const dam = new DamerauLevenshtein();
             const distance = dam.distance('abcd', 'cbad');
             expect(distance).equal(2);
         });
-
+        // 2 insertions
         it('should return 2 for ace - abcde and (0.75, 1, 1.25, 1.5)', function()
         {   
             const dam = new DamerauLevenshtein();
             const distance = dam.distance('ace', 'abcde', {deletionCost:0.75, insertionCost:1,substitutionCost:1.25,transpositionCost:1.5});
             expect(distance).equal(2);
         });
+        // 3 substitutions
         it('should return 3 for abc - def', function()
         {   
             const dam = new DamerauLevenshtein();
@@ -29,37 +45,55 @@ describe('Damerau - Levenshtein Distance', function()
 
     describe('Similarity test', function()
     {
+        // 1 transposition and 1 deletion (unrestricted)
+        it('should return 0.3333 for abc - ca', function()
+        {   
+            const dam = new DamerauLevenshtein();
+            const similarity = dam.similarity('abc', 'ca');
+            const expApp = Math.abs(similarity - 0.3333) < ERROR;
+            expect(expApp).equal(true);
+        });
+        
         it('should return 0.5 for abcd - cbad', function()
         {   
             const dam = new DamerauLevenshtein();
             const similarity = dam.similarity('abcd', 'cbad');
-            const expApp = (0.5-ERROR < similarity && similarity < 0.5+ERROR) ? true : false
+            const expApp = Math.abs(similarity - 0.5) < ERROR;
             expect(expApp).equal(true);
         });
-
+        
         it('should return 0.6522 for ace - abcde and (0.75, 1, 1.25, 1.5)', function()
         {   
             const dam = new DamerauLevenshtein();
             const similarity = dam.similarity('ace', 'abcde', {deletionCost:0.75, insertionCost:1,substitutionCost:1.25,transpositionCost:1.5});
-            const expApp = (0.6522-ERROR < similarity && similarity < 0.6522+ERROR) ? true : false
+            const expApp = Math.abs(similarity - 0.6522) < ERROR;
             expect(expApp).equal(true);
         });
     })
     describe('Normalized test', function()
     {
+        // 1 transposition and 1 deletion (unrestricted)
+        it('should return 0.6667 for abc - ca', function()
+        {   
+            const dam = new DamerauLevenshtein();
+            const normalizedDistance = dam.normalizedDistance('abc', 'ca');
+            const expApp = Math.abs(normalizedDistance - 0.6667) < ERROR;
+            expect(expApp).equal(true);
+        });
+        
         it('should return 0.5 for abcd - cbad', function()
         {   
             const dam = new DamerauLevenshtein();
-            const similarity = dam.normalizedDistance('abcd', 'cbad');
-            const expApp = (0.5-ERROR < similarity && similarity < 0.5+ERROR) ? true : false
+            const normalizedDistance = dam.normalizedDistance('abcd', 'cbad');
+            const expApp = Math.abs(normalizedDistance - 0.5) < ERROR;
             expect(expApp).equal(true);
         });
-
+        
         it('should return 0.3478 for ace - abcde and (0.75, 1, 1.25, 1.5)', function()
         {   
             const dam = new DamerauLevenshtein();
-            const similarity = dam.normalizedDistance('ace', 'abcde', {deletionCost:0.75, insertionCost:1,substitutionCost:1.25,transpositionCost:1.5});
-            const expApp = (0.3478-ERROR < similarity && similarity < 0.3478+ERROR) ? true : false
+            const normalizedDistance = dam.normalizedDistance('ace', 'abcde', {deletionCost:0.75, insertionCost:1,substitutionCost:1.25,transpositionCost:1.5});
+            const expApp = Math.abs(normalizedDistance - 0.3478) < ERROR;
             expect(expApp).equal(true);
         });
     })
